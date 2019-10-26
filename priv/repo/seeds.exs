a3998750cde6f23150b2def878add311301b23d6
Oscar Bernardini <bernardini687@gmail.com>
2019-10-26 16:10:52 +0200

    add seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,59 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     EventManager.Repo.insert!(%EventManager.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+import Ecto.Query, only: [from: 2]
+
+alias EventManager.Users.User
+alias EventManager.Events.Event
+alias EventManager.Attendances.Attendance
+alias EventManager.Repo
+
+create_user = fn tag ->
+  %User{
+    id: Ecto.UUID.generate(),
+    email: "email#{tag}",
+    name: "name#{tag}",
+    username: "username#{tag}",
+    first_name: "first_name#{tag}",
+    last_name: "last_name#{tag}"
+  }
+  |> Repo.insert!()
+end
+
+create_event = fn tag, creator ->
+  %Event{
+    creator: creator,
+    title: "title#{tag}",
+    description: "description#{tag}",
+    location: "location#{tag}",
+    status: :published,
+    start_time: DateTime.utc_now() |> DateTime.truncate(:second),
+    end_time: DateTime.utc_now() |> DateTime.truncate(:second)
+  }
+  |> Repo.insert!()
+end
+
+first_by = fn model, field ->
+  where = [{field, "#{field}1"}]
+  Repo.one(from resource in model, where: ^where)
+end
+
+Repo.delete_all(Attendance)
+Repo.delete_all(Event)
+Repo.delete_all(User)
+
+Enum.each(1..5, &create_user.(&1))
+
+creator = first_by.(User, :name)
+Enum.each(1..5, &create_event.(&1, creator))
+
+[_creator | others] = Repo.all(User)
+event1 = first_by.(Event, :title)
+
+Enum.each(
+  others,
+  fn other ->
+    %Attendance{
+      attendant: other,
+      event: event1
+    }
+    |> Repo.insert!()
+  end
+)
